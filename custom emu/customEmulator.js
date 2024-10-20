9bb275a3252882c7a262aa893103849ccc5303a9
oklmcmoi <oklmcrypto@gmail.com>
2024-10-20 16:30:17 +0200

    Résolution problemes

--- custom emu/customEmulator.js
+++ custom emu/customEmulator.js
@@ -2,15 +2,26 @@ class Emulator {
     constructor(canvas, romData) {
         this.canvas = canvas;
         this.romData = romData;
-        // Initialisation du contexte graphique et de l'émulateur
+        this.ctx = this.canvas.getContext('2d');
+        this.isRunning = false;
     }
 
     start() {
-        // Logique pour démarrer l'émulation de la ROM
-        console.log("L'émulateur démarre !");
-        // Par exemple, dessiner sur le canvas
-        const context = this.canvas.getContext("2d");
-        context.fillStyle = "#000";
-        context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        if (!this.isRunning) {
+            this.isRunning = true;
+            console.log("Émulateur démarré avec ROM :", this.romData);
+            this.updateScreen();
+        }
+    }
+
+    updateScreen() {
+        // Exemple de mise à jour de l'écran avec une couleur simple
+        this.ctx.fillStyle = "#000000";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    stop() {
+        this.isRunning = false;
+        console.log("Émulateur arrêté");
     }
 }
